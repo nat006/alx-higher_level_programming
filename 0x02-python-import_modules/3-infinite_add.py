b9ff57b376f35a8f98bcbc6b5c0425554a18bd47
nat006 <nthatipmoloi006@gmail.com>
2023-11-02 16:12:21 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-args = sys.argv[1:]
-result = 0
-for arg in args:
-    result += int(arg)
-
-print(result)
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    result = 0
+    for arg in args:
+        result += int(arg)
+        print(result)
