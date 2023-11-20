b7a0c04ecd47c7a89c6e8b3b3ecb36252a2af75c
nat006 <nthatipmoloi006@gmail.com>
2023-11-20 10:42:13 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exception: {}".format(sys.exc_info()[0]), file=sys.stderr)
+        return False
