3bb2b7cfb8aa89cc05e0306574ca02cbd89b07d8
nat006 <nthatipmoloi006@gmail.com>
2023-11-21 15:16:40 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 class Square:
-    """A class that defines a square."""
-    
-    def __init__(self):
-        """Initialize a square object."""
-        pass
-
-    def get_area(self):
-        """Calculate the area of the square."""
-        pass
-
-    def get_perimeter(self):
-        """Calculate the perimeter of the square."""
-        pass
-
-    def is_square(self):
-        """Check if the given object is a square."""
-        pass
-
-    def is_regular(self):
-        """Check if the given square is regular (all sides are equal)."""
-        pass
-
-    def display(self):
-        """Display the square's properties."""
-        pass
+    """Represents a square."""
+    pass
