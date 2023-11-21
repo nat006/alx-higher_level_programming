8f2096392993862bf0674ac348e7d378a6318043
nat006 <nthatipmoloi006@gmail.com>
2023-11-21 15:24:04 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """ Initializes a new instance of the Square class.
+         Args:
+            side_length (float): The length of each side of the square.
+            """
         self.__size = size
