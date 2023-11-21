d0f1ff2ca6221602c4902cdda0802ecfa5b60bbf
nat006 <nthatipmoloi006@gmail.com>
2023-11-21 15:56:32 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """  Initializes a new instance of the Square class.
+        
+        Args:
+            size (int, optional): The size of each side of the square. Defaults to 0.
+        """
         self.size = size
 
     @property
     def size(self):
+        """ Getter method to retrieve the size of the square."""
         return self.__size
 
     @size.setter
@@ -17,4 +28,5 @@ class Square:
             self.__size = value
 
     def area(self):
+        """Return area of the square."""
         return self.__size ** 2
