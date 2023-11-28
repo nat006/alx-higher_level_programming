25b418f5c27aa1a8436a1e4fc2dbb072a90d8735
nat006 <nthatipmoloi006@gmail.com>
2023-11-28 06:02:06 -0800

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    result = ""
-    for i in range(1, n+1):
-        result += "BestSchool" * i
-    return result
+    n = 5 # Replace the desired value for n
+    return ''.join(['BestSchool' * i for i in range(1, n+1)])
