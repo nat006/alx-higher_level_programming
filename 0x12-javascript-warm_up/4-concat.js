1bbe6a8b0fbc62ccf416e452413f96a33cc0511d
nat006 <nthatipmoloi006@gmail.com>
2024-02-12 10:32:19 -0800

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const [, , arg1, arg2] = process.argv;
-
-if (!arg1 || !arg2) {
-  console.log('Please provide two arguments');
-} else {
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
