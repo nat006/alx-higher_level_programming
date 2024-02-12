70ee11f2850f34017921148dd468c2b5691d6598
nat006 <nthatipmoloi006@gmail.com>
2024-02-12 10:38:15 -0800

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function findSecondLargest(args) {
+  if (args.length === 0 || args.length === 1) {
+    return 0;
+  }
+
+  const intArgs = args.map(arg => parseInt(arg));
+  intArgs.sort((a, b) => b - a);
+  const uniqueSortedArgs = [...new Set(intArgs)]; // Remove duplicates
+  return uniqueSortedArgs[1];
+}
+
+const [ , , ...args ] = process.argv;
+
+console.log(findSecondLargest(args));
