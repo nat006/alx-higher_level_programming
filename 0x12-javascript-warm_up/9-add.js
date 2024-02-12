abe97e221057d88734829e5f4cd051601b293502
nat006 <nthatipmoloi006@gmail.com>
2024-02-12 10:34:00 -0800

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-const [ , , num1, num2 ] = process.argv;
-const int1 = parseInt(num1);
-const int2 = parseInt(num2);
-
-if (!Number.isNaN(int1) && !Number.isNaN(int2)) {
-  console.log(add(int1, int2));
-} else {
-  console.log("Invalid input, please provide two integers as arguments");
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
