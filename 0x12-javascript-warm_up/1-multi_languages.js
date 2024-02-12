4b6a2e27f999540db3327244cc96b6ee4a771775
nat006 <nthatipmoloi006@gmail.com>
2024-02-12 07:39:03 -0800

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const cMessage = "C is fun";
-const pythonMessage = "Python is cool";
-const jsMessage = "JavaScript is amazing";
+const cMessage = 'C is fun';
+const pythonMessage = 'Python is cool';
+const jsMessage = 'JavaScript is amazing';
 
 console.log(cMessage);
 console.log(pythonMessage);
