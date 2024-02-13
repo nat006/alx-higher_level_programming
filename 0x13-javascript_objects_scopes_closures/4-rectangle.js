a245d090b619d8ae18234212f5c876f861d26fea
nat006 <nthatipmoloi006@gmail.com>
2024-02-13 02:35:29 -0800

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,51 +2,32 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}; // Return an empty object if conditions are met
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 
   print () {
-    if (this.width === undefined || this.height === undefined) {
-      console.log('Empty Rectangle');
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
-        console.log(row);
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
+      console.log(s);
     }
   }
 
   rotate () {
-    if (this.width !== undefined && this.height !== undefined) {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
 
   double () {
-    if (this.width !== undefined && this.height !== undefined) {
-      this.width *= 2;
-      this.height *= 2;
-    }
+    this.width *= 2;
+    this.height *= 2;
   }
 }
-
-// Example usage:
-const rect = new Rectangle(4, 3);
-rect.print();
-rect.rotate();
-console.log('After rotating:');
-rect.print();
-rect.double();
-console.log('After doubling:');
-rect.print();
 
 module.exports = Rectangle;
