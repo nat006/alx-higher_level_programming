a3b1349cdfb2b1a84ab7f77c0fff3f45bc8c6022
nat006 <nthatipmoloi006@gmail.com>
2024-02-13 02:38:15 -0800

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,53 +1,11 @@
 #!/usr/bin/node
-'use strict';
 
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}; // Return an empty object if conditions are met
-    }
-    this.width = w;
-    this.height = h;
-  }
-
-  print () {
-    if (this.width === undefined || this.height === undefined) {
-      console.log('Empty Rectangle');
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
-        console.log(row);
-      }
-    }
-  }
-
-  rotate () {
-    if (this.width !== undefined && this.height !== undefined) {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  }
-
-  double () {
-    if (this.width !== undefined && this.height !== undefined) {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size); // Call the constructor of Rectangle with the given size for both width and height
+    super(size, size);
   }
 }
-
-// Example usage for Square
-const square = new Square(5);
-square.print();
 
 module.exports = Square;
