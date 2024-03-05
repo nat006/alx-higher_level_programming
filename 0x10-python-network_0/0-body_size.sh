6c35b76ba163feec2b6ac2e0e122eb0679162030
nat006 <nthatipmoloi006@gmail.com>
2024-03-05 13:25:21 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Script to send a request to a URL and display the size of the response body in bytes
+# Bash script to send request to URL and display the size of the response body
 
-# Check if URL is provided as argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 [URL]"
-    exit 1
+# Check if URL argument is provided
+if [-z "$1"] then
+echo "Usage: $0 <URL>"
+exit 1
 fi
 
-# Send request to URL and store the response body in a temporary file 
-response=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
+# Send request to URL and store response in a variable
+response = $(curl - sI "$1")
 
-# Display the size of the response body in bytes
-echo "Response Body Size: $response bytes"
+# Extract Content-Length header from response
+content_length = $(echo "$response" |
+grep - i "Content-Length" |
+awk '{print $2}')
