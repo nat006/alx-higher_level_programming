5fe388441dec821be7ee6f3407b8e37a5661b2ef
nat006 <nthatipmoloi006@gmail.com>
2024-03-26 07:54:52 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    let wedgeMovies = 0;
-    films.forEach((film) => {
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        wedgeMovies++;
-      }
-    });
-    console.log(wedgeMovies);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
