9fb11f58ec5f7c87f44ea20536c17f00f8e5bd78
nat006 <nthatipmoloi006@gmail.com>
2024-03-26 08:05:30 -0700

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    for (const userId in completedTasksByUser) {
+      console.log(`User ID ${userId} completed ${completedTasksByUser[userId]} tasks`);
+    }
+  }
+});
